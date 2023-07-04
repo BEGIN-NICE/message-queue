812be8313e7c84b917509c144a3d0e92c9347686
fanxiaohui <xiaohui.fxh@alibaba-inc.com>
2023-07-04 18:40:52 +0800

    Kafka入门

---
+++ kafka-springboot/src/main/java/com/fxh/kafka/Application.java
@@ -0,0 +1,17 @@
+package com.fxh.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author FanXH
+ * @Date 2023/6/28 20:07
+ * @Description
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
